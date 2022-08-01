011b26a6a54f4fdabda0a97d445ce2b78effb904
Jerry <jerrykress.wsj@gmail.com>
2022-08-01 23:42:43 +0100

    Update Ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jerry/Code/Xcurse
-# Build directory: /home/jerry/Code/Xcurse/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
