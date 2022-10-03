27ccf5cc10a64f016d9fa04d62fcbc9d9b0561de
Jerry <jerrykress.wsj@gmail.com>
2022-10-03 14:45:07 +0100

    TextField Align

--- main.cpp
+++ main.cpp
@@ -13,12 +13,18 @@ int main(int, char **)
     Display &d = *Display::get_display();
     d.set_refresh_interval(200);
 
-    bool add_wig0 = d.add_obj("root", "t1", new TextField("t1", L"Program", ALIGN_CENTER));
+    bool add_wig0 = d.add_obj("root", "t1", new TextField("t1", L"Program", ALIGN_RIGHT));
     bool add_win0 = d.add_obj("root", "v1", new Layout("v1", Horizontal, 1));
     bool add_win3 = d.add_obj("root", "w1", new TrendChartWindow("w1", 1));
     bool add_win1 = d.add_obj("v1", "w2", new GridWindow("w2", 1));
     bool add_win2 = d.add_obj("v1", "w3", new BarChartWindow("w3", 1));
 
+    // setup win
+    auto text_widget = static_cast<TextField *>(d["t1"]);
+    text_widget->background = BACKGROUND_COLOR_BLUE;
+    text_widget->bold = true;
+
+    // setup win
     TrendChartWindow *trend_win = static_cast<TrendChartWindow *>(d["w1"]);
     std::vector<float> d_low = {1, 1, 3, 0, 0};
     std::vector<float> d_high = {2, 3, 5, 4, 2};
@@ -26,10 +32,12 @@ int main(int, char **)
     trend_win->set_data(d_low, d_high, d_ref);
     trend_win->set_title(L" Trend ");
 
+    // setup win
     GridWindow *win = static_cast<GridWindow *>(d["w2"]);
     win->background = BACKGROUND_COLOR_BRIGHT_YELLOW;
     win->set_title(L" Grid ");
 
+    // setup win
     BarChartWindow *bar_win = static_cast<BarChartWindow *>(d["w3"]);
     std::vector<float> ds = {1, 2, 3, 2, 1, 2, 3};
     bar_win->set_data(ds);
--- src/Widgets/TextField.cpp
+++ src/Widgets/TextField.cpp
@@ -24,11 +24,58 @@ namespace Xcurse
 
     void TextField::draw()
     {
-        const int margin_left = (m_size.width - m_data.size()) / 2;
+        const int margin_all = m_size.width - m_data.size();
+        const int margin_left = margin_all / 2;
+        const int margin_right = margin_all - margin_left;
 
-        for (int i = 0; i < m_data.size(); i++)
+        switch (alignment)
         {
-            m_display_ptr->set_pixel(this, i, 0, Pixel(m_data[i], Stylable(TEXT_COLOR_BRIGHT_WHITE, BACKGROUND_COLOR_BLUE, false, false, false)));
+        case ALIGN_LEFT:
+            // print content
+            for (int i = 0; i < m_data.size(); i++)
+            {
+                m_display_ptr->set_pixel(this, i, 0, Pixel(m_data[i], *this));
+            }
+            // print rest of line
+            for (int i = m_data.size(); i < m_size.width; i++)
+            {
+                m_display_ptr->set_pixel(this, i, 0, Pixel(L' ', *this));
+            }
+            break;
+
+        case ALIGN_CENTER:
+            // print left margin
+            for (int i = 0; i < margin_left; i++)
+            {
+                m_display_ptr->set_pixel(this, i, 0, Pixel(L' ', *this));
+            }
+            // print content
+            for (int i = 0; i < m_data.size(); i++)
+            {
+                m_display_ptr->set_pixel(this, i + margin_left, 0, Pixel(m_data[i], *this));
+            }
+            // print right margin
+            for (int i = m_size.width - margin_right; i < m_size.width; i++)
+            {
+                m_display_ptr->set_pixel(this, i, 0, Pixel(L' ', *this));
+            }
+            break;
+
+        case ALIGN_RIGHT:
+            // print leading pixels
+            for (int i = 0; i < margin_all; i++)
+            {
+                m_display_ptr->set_pixel(this, i, 0, Pixel(L' ', *this));
+            }
+            // print content
+            for (int i = 0; i < m_data.size(); i++)
+            {
+                m_display_ptr->set_pixel(this, i + margin_all, 0, Pixel(m_data[i], *this));
+            }
+            break;
+
+        default:
+            break;
         }
     }
 }
